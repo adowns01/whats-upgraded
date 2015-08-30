397df554cd4622b5aa930cba8892c9c9f0d9c853
adowns01 <downs.amelia@gmail.com>
2015-08-29 19:02:50 -0700

    show ember and rails info

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,21 @@
 class ProjectsController < ApplicationController
 	def index
 		mechanize = Mechanize.new
-		ember = "http://emberjs.com/blog/tags/releases.html"
-		page = mechanize.get(ember)
+		github_target = ".release.label-latest .release-meta .css-truncate-target"
 
-		@ember_release = page.at("h2 a").text.strip
+		ember_cli = "https://github.com/ember-cli/ember-cli/releases"
+		ember_cli_page = mechanize.get(ember_cli)
+		@ember_cli_release = ember_cli_page.at(github_target).text.strip
+
+		ember = "https://github.com/emberjs/ember.js/releases"
+		ember_page = mechanize.get(ember)
+		@ember_release = ember_page.at(github_target).text.strip
+
+		rails_github_target = ".tag-info .tag-name"
+		rails = "https://github.com/rails/rails/releases"
+		rails_page = mechanize.get(rails)
+		@rails_release = rails_page.at(rails_github_target).text.strip
+
 	end 
 
 end
