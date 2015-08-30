72a4242320382b7b660ab999930182c692c2e1db
adowns01 <downs.amelia@gmail.com>
2015-08-30 16:51:09 -0700

    display project versions using model method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,21 +1,5 @@
 class ProjectsController < ApplicationController
 	def index
-		mechanize = Mechanize.new
-		github_target = ".release.label-latest .release-meta .css-truncate-target"
-
-		ember_cli = "https://github.com/ember-cli/ember-cli/releases"
-		ember_cli_page = mechanize.get(ember_cli)
-		@ember_cli_release = ember_cli_page.at(github_target).text.strip
-
-		ember = "https://github.com/emberjs/ember.js/releases"
-		ember_page = mechanize.get(ember)
-		@ember_release = ember_page.at(github_target).text.strip
-
-		rails_github_target = ".tag-info .tag-name"
-		rails = "https://github.com/rails/rails/releases"
-		rails_page = mechanize.get(rails)
-		@rails_release = rails_page.at(rails_github_target).text.strip
-
+		@projects = Project.all
 	end 
-
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ActiveRecord::Base
+
+	def self.mechanize
+		return @@mechanizer if defined? @@mechanizer
+		@@mechanizer = Mechanize.new
+	end
+
+	def latest_version 
+		page = Project.mechanize.get(github_url)
+		return page.at(selector).text.strip
+	end 
 end
+
